0214e874258374f6466474988c0a091917cbe1fa
Ryan <degruyter.ryan@gmail.com>
2015-11-20 20:20:24 +0100

    added itemdecorator

---
+++ app/src/main/java/be/ryan/popularmovies/ui/fragment/ItemOffsetDecoration.java
@@ -0,0 +1,32 @@
+package be.ryan.popularmovies.ui.fragment;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.support.annotation.DimenRes;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * code copy and pasted from stackoverflow
+ * author yqritc
+ * http://stackoverflow.com/questions/28531996/android-recyclerview-gridlayoutmanager-column-spacing
+ */
+public class ItemOffsetDecoration extends RecyclerView.ItemDecoration {
+    private int mItemOffset;
+
+    public ItemOffsetDecoration(int itemOffset) {
+        mItemOffset = itemOffset;
+    }
+
+    public ItemOffsetDecoration(@NonNull Context context, @DimenRes int itemOffsetId) {
+        this(context.getResources().getDimensionPixelSize(itemOffsetId));
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent,
+                               RecyclerView.State state) {
+        super.getItemOffsets(outRect, view, parent, state);
+        outRect.set(mItemOffset, mItemOffset, mItemOffset, mItemOffset);
+    }
+}
